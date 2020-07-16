b677266630e02d887f1367c8f9b41b818417f109
Rachel <rjcrter11@gmail.com>
2020-07-16 18:33:10 -0500

    getting close to mvp for sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,59 +4,113 @@ from world import World
 
 import random
 from ast import literal_eval
+from collections import deque
 
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_line.txt"
+#map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+#map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
 
 player = Player(world.starting_room)
+start = player.current_room
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
 traversal_path = []
 
+reverse_traversal = []
+
+visited = {}
+
+go_back = {
+    'n': 's',
+    's': 'n',
+    'e': 'w',
+    'w': 'e'
+}
+current_room = start
 
 
+if current_room.id not in visited:
+
+    room_exits = current_room.get_exits()
+    print("Exits: ", room_exits)
+    exits = {}
+    for ext in room_exits:
+        exits[ext] = "?"
+        visited[current_room.id] = exits
+    print("Visited: ", visited)
+
+while len(visited) < len(room_graph) - 1:
+    current = current_room.id
+    exit_room = visited[current]
+    print("Exits in while loop", exit_room)
+
+    for move in exit_room:
+        if exit_room[move] == '?' and current_room.get_room_in_direction(move) not in visited:
+            next_room = move
+        else:
+            next_room = None
+    if next_room is None:
+        break  # go backwards to look for an exit
+    else:
+        player.travel(next_room)
+        traversal_path.append(next_room)
+
+        reverse_traversal.append(go_back[next_room])
+        prev_room = current_room
+        current_room = player.current_room
+        print("Previous room: ", prev_room.id)
+
+        next_exits = {}
+        for ext in current_room.get_exits():
+            next_exits[ext] = '?'
+            visited[current_room.id] = next_exits
+
+
+print("Traversal_path", traversal_path)
+print("Visited: ", visited)
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+
 
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,5 +1,6 @@
 import random
 from collections import deque
+import time
 
 
 class User:
@@ -18,12 +19,13 @@ class SocialGraph:
         Creates a bi-directional friendship
         """
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
-            print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -80,6 +82,32 @@ class SocialGraph:
         for friendship in random_friendships:
             self.add_friendship(friendship[0], friendship[1])
 
+    def linear_populate_graph(self, num_users, avg_friendships):
+        # Reset graph
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+        # !!!! IMPLEMENT ME
+
+        # Add users
+        # use num_users
+        for user in range(num_users):
+            self.add_user(user)
+
+        # linear way to add number of friendships we need
+        # as long as we haven't made al lthe friendships we need
+        target_number_friendships = num_users * avg_friendships
+        friendships_created = 0
+        while friendships_created < target_number_friendships:
+            # pick 2 random numbers btw 1 and last_id
+            friend_one = random.randint(1, self.last_id)
+            friend_two = random.randint(1, self.last_id)
+        # try to create that friendship
+            friendship_was_made = self.add_friendship(friend_one, friend_two)
+        # if we can, increment friendship counter by 2
+            if friendship_was_made:
+                friendships_created += 2
+
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
@@ -88,6 +116,7 @@ class SocialGraph:
         extended network with the shortest friendship path between them.
 
         The key is the friend's ID and the value is the path.
+        Plan: BFT, use dict as visited
         """
         # visited is graph
         # Node: users and users friends
@@ -96,28 +125,82 @@ class SocialGraph:
 
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        # q = deque()
+        # path = [user_id]
+        # q.append(path)
+
+        # while len(q) > 0:
+        #     path = q.popleft()
+        #     user = path[-1]
+
+        #     if user not in visited:
+        #         visited[user] = path
+
+        #         for friend in self.friendships[user]:
+        #             new_path = list(path)
+        #             new_path.append(friend)
+        #             q.append(new_path)
+
         q = deque()
-        path = [user_id]
-        q.append(path)
+        q.append([user_id])
 
-        while len(q) > 0:
-            path = q.popleft()
-            user = path[-1]
+        # While q not empty
+        while len(q):
+            # dequeue current path
+            current_path = q.popleft()
 
-            if user not in visited:
-                visited[user] = path
+            # grab last vertex from path
+            current_user = current_path[-1]
+            # if not been visited
+            if current_user not in visited:
+                # add to dictionary
+                visited[current_user] = current_path
+            # { friend_id: path }
+                friends = self.friendships[current_user]
+            # then enqueue path to each of our neighbors
+                for friend in friends:
+                    path_to_friend = current_path + [friend]
 
-                for friend in self.friendships[user]:
-                    new_path = list(path)
-                    new_path.append(friend)
-                    q.append(new_path)
+                    q.append(path_to_friend)
 
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print("Friendships: ", sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print("Connections: ", connections)
+
+    num_users = 10000
+    avg_friendships = 5
+
+    # start_time = time.time()
+    # sg.populate_graph(num_users, avg_friendships)
+    # end_time = time.time()
+    # print(f"Pop graphO(n^2): {end_time - start_time}")
+    start_time = time.time()
+    sg.linear_populate_graph(num_users, avg_friendships)
+    end_time = time.time()
+    print(f"Pop graph O(n): {end_time - start_time}")
+
+    # print("Friendships: ", sg.friendships)
+    #connections = sg.get_all_social_paths(1)
+    # print("Connections: ", connections)
+
+    # what percentage of total users are in our extended social network?
+
+    # how many people we know, divided by how many people there are
+
+    #print(f'{(len(connections) - 1) / 10000 * 100}%')
+
+    # Whats the avg degree of separation btw a user and those in his/her extended network?
+    # Avg length of each path in connections/ avg length of a path to each user
+    # traverse a user's extended connections, gather lengths, sum,
+    total_lengths = 0
+    # for friend in connections:
+    # n = [len(v) for k, v in connections.items() if k != 1]
+    # print(sum(n) / len(n))
+    # sum([len(connection) for connection in connections])/len(connections)
+    #total_lengths += len(connections[friend])
+    # divide by number of friends in connected component aka extended social network
+
+    # print(
+    # f'Average degree of separation: {total_lengths / len(connections)}')
